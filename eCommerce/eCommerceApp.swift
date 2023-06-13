3f6352e9cc0d7c33e2f3fedbd80560ef2899b105
Imen Ksouri <imen_ksouri@hotmail.fr>
2023-06-13 23:58:53 +0200

    Initial Commit

---
+++ eCommerce/eCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceApp.swift
+//  eCommerce
+//
+//  Created by Imen Ksouri on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
